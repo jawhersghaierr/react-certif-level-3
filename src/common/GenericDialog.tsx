7e913909971beb1d72c71f15346415c58f8accd3
sghaier jawhar <jawhar.sghaier@elite-group.fr>
2023-10-30 17:25:12 +0100

    fix

--- src/common/GenericDialog.tsx
+++ src/common/GenericDialog.tsx
@@ -23,9 +23,6 @@ const Dialog: React.FC<DialogProps> = ({
     left: 0,
     right: 0,
     bottom: 0,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
     zIndex: 9999,
     backgroundColor: "rgba(0, 0, 0, 0.5)",
   };
